0c9e7965b1ccd7523d1addcb31b12b72959038b7
sourceperl <pi@raspberrypi.(none)>
2013-03-29 15:22:27 +0100

    first release of readme.pod, update test.pl for compat. with doc

--- test.pl
+++ test.pl
@@ -6,22 +6,22 @@ use strict;
 use MBclient;
 
 # create modbus object
-my $mb = MBclient->new();
+my $m = MBclient->new();
 
 # on local modbus server
-$mb->host("127.0.0.1");
-$mb->unit_id(1);
+$m->host("127.0.0.1");
+$m->unit_id(1);
 # for print frame and debug string : uncomment this line
-#$mb->{debug} = 1;
+#$m->{debug} = 1;
 
 # open TCP socket
-if (! $mb->open()) {
+if (! $m->open()) {
   print "unable to open TCP socket.\n";
   exit(1);
 }
 
 # read register 0 to 9 and print it on stdout
-my $words = $mb->read_holding_registers(0, 10);
+my $words = $m->read_holding_registers(0, 10);
 foreach my $word (@$words) {
   print $word."\n";
 }
